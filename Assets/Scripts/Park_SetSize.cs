1c6fc08425edee0d322ddfb42bb604c812006d98
samisaeed789 <164140197+samisaeed789@users.noreply.github.com>
2025-01-09 17:56:32 +0500

    Working on Parking Mode

--- Assets/Scripts/Park_SetSize.cs
+++ Assets/Scripts/Park_SetSize.cs
@@ -4,5 +4,56 @@ using UnityEngine;
 
 public class Park_SetSize : MonoBehaviour
 {
+    public GameObject[] props;
+    public Vector3 propsSize = new Vector3(1f, 1f, 1f);
+
+    public GameObject[] concreteBarr;
+    public Vector3 concreteBarrSize = new Vector3(1f, 1f, 1f);
     
+    public GameObject[] rodBarr;
+    public Vector3 rodBarrSize = new Vector3(1f, 1f, 1f);
+
+    public GameObject[] Cones;
+    public Vector3 conesSize = new Vector3(1f, 1f, 1f);
+
+    public GameObject[] Lamberts;
+    public Vector3 lambertsSize = new Vector3(1f, 1f, 1f);
+
+    public GameObject[] tyres;
+    public Vector3 tyresSize = new Vector3(1f, 1f, 1f);
+
+    public GameObject[] Containers;
+    public Vector3 containersSize = new Vector3(1f, 1f, 1f);
+
+    public GameObject[] barrells;
+    public Vector3 barrellsSize = new Vector3(1f, 1f, 1f);
+
+    public GameObject[] crate_lambert;
+    public Vector3 crateLambertSize = new Vector3(1f, 1f, 1f);
+
+
+    void Start()
+    {
+        SetSizeForObjects(concreteBarr, concreteBarrSize);
+        SetSizeForObjects(rodBarr, rodBarrSize);
+        SetSizeForObjects(Cones, conesSize);
+        SetSizeForObjects(Lamberts, lambertsSize);
+        SetSizeForObjects(tyres, tyresSize);
+        SetSizeForObjects(Containers, containersSize);
+        SetSizeForObjects(barrells, barrellsSize);
+        SetSizeForObjects(crate_lambert, crateLambertSize);
+    }
+
+    void SetSizeForObjects(GameObject[] objects, Vector3 size)
+    {
+        foreach (GameObject obj in objects)
+        {
+            if (obj != null)
+            {
+                obj.transform.localScale = size;
+            }
+        }
+    }
 }
+
+
